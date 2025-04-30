4e33a439ea04dfe354807f3332b47bb671ac3b2a
jdv <julien@crowdsec.net>
2025-04-30 18:15:58 +0200

    deploy test02

--- crowdsec/scripts/deploy_crowdsec.sh
+++ crowdsec/scripts/deploy_crowdsec.sh
@@ -9,10 +9,9 @@ fi
 echo "Deploying CrowdSec" >> $DEPLOY_LOG_FILE
 # Define the base directory
 
-# cd $INSTALL_DIR
-# wget "https://github.com/crowdsecurity/crowdsec/releases/download/${CROWDSEC_VERSION}/crowdsec-release.tgz"
-# tar -xvzf crowdsec-release.tgz
-# cp ${HOME}/scripts/crowdsec-setup.sh "crowdsec-${CROWDSEC_VERSION}/"
-# cd "crowdsec-${CROWDSEC_VERSION}"
-# bash crowdsec-setup.sh /app $CROWDSEC_DIR
-
+cd $TMP_DIR
+wget "https://github.com/crowdsecurity/crowdsec/releases/download/${CROWDSEC_VERSION}/crowdsec-release.tgz"
+tar -xvzf crowdsec-release.tgz
+cp ${SCRIPTS_DIR}/crowdsec-setup.sh "crowdsec-${CROWDSEC_VERSION}/"
+cd "crowdsec-${CROWDSEC_VERSION}"
+bash crowdsec-setup.sh $CROWDSEC_DIR
