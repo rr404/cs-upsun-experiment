18cc18127d6bbfdd4626532c3bed45f91b728b81
jdv <julien@crowdsec.net>
2025-05-02 11:45:06 +0200

    adding back one WP site

--- crowdsec/scripts/systemd_install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# -*- coding: utf-8 -*-
-
-echo "Create Systemd user folder structure..."
-mkdir -p ~/.config/systemd/user/multi-user.target.wants/
-
-echo "Copy Systemd user services..."
-cp -R scripts/systemd.d/* ~/.config/systemd/user/
-cp -R scripts/systemd.d/* ~/.config/systemd/user/multi-user.target.wants/
