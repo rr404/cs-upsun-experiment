8f85d27d476ab190d8b363ecf0cce3da8e100bdf
jdv <julien@crowdsec.net>
2025-06-25 11:40:54 +0200

    starting rsyslog service

--- crowdsec/scripts/start-crowdsec-service.sh
+++ crowdsec/scripts/start-crowdsec-service.sh
@@ -3,14 +3,11 @@
 echo "Reload Services"
 systemctl --user daemon-reload
 
-echo "Activating with user"
+echo "Activating crowdsec with user"
 systemctl --user enable crowdsec.service
 
-echo "Starting with user"
+echo "Starting crowdsec with user"
 systemctl --user start crowdsec.service
-
-# echo "Show current services"
-# systemctl --user
 
 echo "Show current services"
 systemctl --user status crowdsec.service
@@ -18,3 +15,16 @@ systemctl --user status crowdsec.service
 echo "checking CAPI user"
 echo $(head -n2 ${CROWDSEC_DIR}/online_api_credentials.yaml | tail -n1)
 
+
+
+echo "Activating rsyslog with user"
+systemctl --user enable rsyslog.service
+
+echo "Starting rsyslog with user"
+systemctl --user start rsyslog.service
+
+# echo "Show current services"
+# systemctl --user
+
+echo "Show current services"
+systemctl --user status rsyslog.service
