72473a0b7023334b96ee3d21c6302218416077c4
jdv <julien@crowdsec.net>
2025-06-26 15:19:45 +0200

    trying syslog again with http 2 udp relay phase 0

--- crowdsec/scripts/start-crowdsec-service.sh
+++ crowdsec/scripts/start-crowdsec-service.sh
@@ -15,6 +15,19 @@ systemctl --user status crowdsec.service
 echo "checking CAPI user"
 echo $(head -n2 ${CROWDSEC_DIR}/online_api_credentials.yaml | tail -n1)
 
+# http2udpRelay
+# echo "Activating http2udpRelay with user"
+# systemctl --user enable http2udpRelay.service
+
+# echo "Starting http2udpRelay with user"
+# systemctl --user start http2udpRelay.service
+
+# echo "Show current service status"
+# systemctl --user status http2udpRelay.service
+
+# rsyslog
+# echo "Show current services"
+# systemctl --user status rsyslog.service
 
 
 # echo "Activating rsyslog with user"
