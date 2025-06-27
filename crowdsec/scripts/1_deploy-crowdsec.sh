0b3683495da24f33b57dacf683c1b3a4ca163641
jdv <julien@crowdsec.net>
2025-06-27 14:03:27 +0200

    removing some old config files from experiments, renaming scripts

--- crowdsec/scripts/1_deploy-crowdsec.sh
+++ crowdsec/scripts/1_deploy-crowdsec.sh
@@ -13,12 +13,19 @@ mkdir -p $LOG_DIR
 mkdir -p $TMP_DIR
 mkdir -p $CROWDSEC_DIR
 
+# Download and setup CrowdSec
 cd $TMP_DIR
 wget "https://github.com/crowdsecurity/crowdsec/releases/download/${CROWDSEC_VERSION}/crowdsec-release.tgz"
 tar -xvzf crowdsec-release.tgz
 cp ${SCRIPTS_DIR}/crowdsec-setup.sh "crowdsec-${CROWDSEC_VERSION}/"
 cd "crowdsec-${CROWDSEC_VERSION}"
-bash crowdsec-setup.sh $CROWDSEC_DIR
+bash 1a_crowdsec-setup.sh $CROWDSEC_DIR
+
+# Copy binaries dependencies
+mkdir -p $BIN_DIR
+cp ${SCRIPTS_DIR}/bin/* $BIN_DIR
+
+# Start CrowdSec service
 cp ${SCRIPTS_DIR}/crowdsec/* -R $CROWDSEC_DIR
 cd ${SCRIPTS_DIR}
-bash start-crowdsec-service.sh >> $DEPLOY_LOG_FILE 2>&1
+bash 1b_start-crowdsec-service.sh >> $DEPLOY_LOG_FILE 2>&1
