1d11e94c016c51092c3bb7e61f76f1d3f6e6bab3
jdv <julien@crowdsec.net>
2025-06-29 09:55:18 +0200

    env var typo fix

--- eng/scripts/cs/0_crowdsec_build.sh
+++ eng/scripts/cs/0_crowdsec_build.sh
@@ -3,7 +3,6 @@
 
 # Setting up Service Files for CrowdSec
 echo "Create Systemd user folder structure..."
-echo "testing env variable: VAR_DIR=${VAR_DIR}"
 mkdir -p ~/.config/systemd/user/multi-user.target.wants/
 
 echo "Copy Systemd user services..."
