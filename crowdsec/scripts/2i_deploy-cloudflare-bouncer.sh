034077324306bf27e2cdb12d7613f46aca139b52
jdv <julien@crowdsec.net>
2025-07-02 11:02:52 +0200

    removed eng app and mini init of cf bouncer script

--- crowdsec/scripts/2i_deploy-cloudflare-bouncer.sh
+++ crowdsec/scripts/2i_deploy-cloudflare-bouncer.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
 echo "Deploying Cloudflare Bouncer"
+CF_BOUNCER_VERSION="v0.0.14"
+
+cd $TMP_DIR
+
+wget "https://github.com/crowdsecurity/cs-cloudflare-worker-bouncer/releases/download/${CF_BOUNCER_VERSION}/crowdsec-cloudflare-worker-bouncer-linux-amd64.tgz"
