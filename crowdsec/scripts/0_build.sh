25473491b75c301c2c21764dd70829c27b20fbc7
jdv <julien@crowdsec.net>
2025-07-02 10:20:05 +0200

    no more http relay needed

--- crowdsec/scripts/0_build.sh
+++ crowdsec/scripts/0_build.sh
@@ -8,8 +8,3 @@ mkdir -p ~/.config/systemd/user/multi-user.target.wants/
 echo "Copy Systemd user services..."
 cp -R scripts/systemd.d/* ~/.config/systemd/user/
 cp -R scripts/systemd.d/* ~/.config/systemd/user/multi-user.target.wants/
-
-# http2udpRelay service build
-cd /app/scripts/bin/http2udpRelay
-echo "Building http2udpRelay..."
-go build -o http2udpRelay main.go
